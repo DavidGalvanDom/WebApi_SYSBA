e735b6c81e8d497b4a17b0311ced301f836a5f23
DavidGalvanDom <davigalva@hotmail.com>
2015-12-31 18:27:51 -0600

    Creacion del prototipo del proyecto

---
+++ WebApi_SYSBA/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_SYSBA.Models
+{
+    public class Usuario
+    {
+        public int usuarioId { get; set; }
+
+        public string nombre { get; set; }
+
+        public string contrasena { get; set; }
+    }
+}
